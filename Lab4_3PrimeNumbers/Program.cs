2ee9e7222b9a8fedcf2c6a520d0c146c4738e515
Adam Rolain <AdamRolain@QuickenLoans.com>
2021-08-16 15:43:18 -0400

    Initialzing repo

---
+++ Lab4_3PrimeNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab4_3PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class PrimeNumbers
+    {
+
+    }
+}
---
+++ PrimeNumbers_Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using static Lab4_3PrimeNumbers.PrimeNumbers;
+
+namespace PrimeNumbers_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
