0184800d8a95a012a7aeb08b2fa7d1abcc91a44f
Adam Rolain <AdamRolain@QuickenLoans.com>
2021-08-16 16:15:48 -0400

    Created IsPrimeNumber() and GetPrime() methods in PrimeNumbers class. Wrote unit tests for each.

--- Lab4_3PrimeNumbers/Program.cs
+++ Lab4_3PrimeNumbers/Program.cs
@@ -6,12 +6,50 @@ namespace Lab4_3PrimeNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Let's locate some primes!\n\nThis application will find you any prime, in order, from first prime number on.\n");
+
+            Console.Write("Which prime number are you looking for? ");
+            int index = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nThe number {index} prime is {PrimeNumbers.GetPrime(index)}");
         }
     }
 
     public class PrimeNumbers
     {
+        // METHODS
+        public static int GetPrime(int n)
+        {
+            int counter = 0;
+            int currentNumber = 2;
 
+            while (counter < n)
+            {
+                if (IsPrimeNumber(currentNumber))
+                {
+                    counter++;
+                }
+                currentNumber++;
+            }
+
+            currentNumber--;
+            return currentNumber;
+        }
+
+        public static bool IsPrimeNumber(int _num)
+        {
+            bool isPrime = true;
+
+            for (double i = _num - 1; i > 1; i--)
+            {
+                double result = _num / i;
+                if (result % 1 == 0)
+                {
+                    isPrime = false;
+                }
+            }
+
+            return isPrime;
+        }
     }
 }
--- PrimeNumbers_Tests/UnitTest1.cs
+++ PrimeNumbers_Tests/UnitTest1.cs
@@ -1,15 +1,35 @@
 using System;
 using Xunit;
-using static Lab4_3PrimeNumbers.PrimeNumbers;
+using Lab4_3PrimeNumbers;
 
 namespace PrimeNumbers_Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(2, true)]
+        [InlineData(3, true)]
+        [InlineData(4, false)]
+        [InlineData(11, true)]
+        [InlineData(60, false)]
+        [InlineData(73, true)]
+        [InlineData(100, false)]
+        public void TestIsPrimeNumber(int _num, bool _expected)
         {
+            Assert.Equal(_expected, PrimeNumbers.IsPrimeNumber(_num));
+        }
 
+        [Theory]
+        [InlineData(1, 2)]
+        [InlineData(2, 3)]
+        [InlineData(5, 11)]
+        [InlineData(21, 73)]
+        [InlineData(42, 181)]
+        [InlineData(204, 1249)]
+
+        public void TestGetPrime(int _num, int _expected)
+        {
+            Assert.Equal(_expected, PrimeNumbers.GetPrime(_num));
         }
     }
 }
